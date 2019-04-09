e3419153d227f24fbcfd5a0a85dde8e32638e776
Kejanu <kurznerk@gmail.com>
2019-04-09 19:29:39 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    int upTo = 100;
+    int upperBound = (int)sqrt(upTo);
+
+    int i = 1;
+    while(i <= 100) {
+        for (int j = 1; j <= upperBound; ++j) {
+            for (int k = 1; k <= upperBound; ++k) {
+                if ((pow(j,2) + pow(k,2)) == i) {
+                    printf("%d kann als %d^2 + %d^2 dargestellt werden\n", i, j ,k);
+                    goto AFTERFOR;
+                }
+            }
+        }
+        AFTERFOR:
+        ++i;
+    }
+
+    return 0;
+}
