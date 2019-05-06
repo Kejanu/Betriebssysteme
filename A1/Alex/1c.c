9ac52a88350d5fb7c217c324102df0dda0b17a57
Kejanu <kurznerk@gmail.com>
2019-05-06 14:20:21 +0200

    Liv's Improvement + Reformating

--- A1/Alex/1c.c
+++ A1/Alex/1c.c
@@ -9,71 +9,63 @@
 #include <string.h>
 
 int main(void) {
+    while (1) {
+        //Abfrage des gewünschten Aufrufs
+
+        int auswahl;
+        printf("\nBitte ein Zahl zur Auswahl eingeben.\n1.ps\n2.ls\n3.date\n4.exit\n");
 
 
-        while (1) {
-
-            //Abfrage des gewünschten Aufrufs
-
-            char auswahl[5];
-            printf("\nBitte ein Zahl zur Auswahl eingeben.\n1.ps\n2.ls\n3.date\n");
-
-
-            if (scanf("%4s", &auswahl) != 1) {
-                printf("Fehler bei scanf!");
-                return 1;
-            }
-
-            if (strncmp("exit", auswahl, 4)==0) {
-                printf("Programm beendet\n");
-                return 0;
-            }
-
-            int zahl = strtol(&auswahl[0], NULL , 10 );
-
-            //fork mit Fehlerabfrage
-
-            pid_t returnValue;
-            returnValue = fork();
-
-            switch (returnValue) {
-                case -1:
-                    perror("fork");
-                    exit(EXIT_FAILURE);
-
-                default:
-                    printf("Child PID is %d\n", returnValue);
-                    pid_t r = waitpid(-1, NULL, 0);
-                    if (r == -1) {
-                        if (errno != ECHILD) {
-                            perror("waitpid");
-                            exit(EXIT_FAILURE);
-                        }
-                    }
-                    break;
-
-                case 0:
-
-                    //Programmaufrufe
-
-                    switch (zahl) {
-
-                        case 1:
-                            execlp("ps", "ps", NULL);
-                            return -1;
-                        case 2:
-                            execlp("ls", "ls", NULL);
-                            return -1;
-                        case 3:
-                            execlp("date", "date", NULL);
-                            return -1;
-                        default:
-                            printf("Ungültige Eingabe\n");
-                            return -1;
-                    }
-
-            }
+        if (scanf("%d", &auswahl) != 1) {
+            printf("Fehler bei scanf!");
+            return 1;
         }
+
+        if (auswahl == 4) {
+            printf("Programm beendet");
+            return 0;
+        }
+
+        //fork mit Fehlerabfrage
+        pid_t returnValue;
+        returnValue = fork();
+
+        switch (returnValue) {
+            case -1:
+                perror("fork");
+                exit(EXIT_FAILURE);
+
+            default:
+                printf("Child PID is %d\n", returnValue);
+                pid_t r = waitpid(-1, NULL, 0);
+                if (r == -1) {
+                    if (errno != ECHILD) {
+                        perror("waitpid");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                break;
+
+            case 0:
+
+                //Programmaufrufe
+                switch (auswahl) {
+
+                    case 1:
+                        execlp("ps", "ps", NULL);
+                        return -1;
+                    case 2:
+                        execlp("ls", "ls", NULL);
+                        return -1;
+                    case 3:
+                        execlp("date", "date", NULL);
+                        return -1;
+                    default:
+                        printf("Ungültige Eingabe\n");
+                        return -1;
+                }
+        }
+    }
 
 }
 
